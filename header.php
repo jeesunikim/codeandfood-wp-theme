09785e19989dca39551a38fb34e402e5f14ee3b7
Jeesun Kim <jeesunikim@users.noreply.github.com>
2023-06-20 22:43:49 -0700

    enable mobile layout and add tags on home

--- header.php
+++ header.php
@@ -19,13 +19,7 @@
 
 	<header>
 		<div class="container mx-auto">
-			<nav class="grid grid-cols-3 max-w-screen-xl mx-auto py-10 px-2">
-				<ul class="flex place-items-center">
-					<li class="flex-auto text-center">
-					<a href="#" class="text-base">about</a>
-					</li>
-				</ul>
-
+			<nav class="max-w-screen-xl mx-auto py-10 px-2">
 				<div class="flex flex-col place-items-center justify-items-center">
 					<h2 class="text-center text-3xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight">
 					<a href="/code" class="hover:underline">Code</a>
@@ -35,19 +29,17 @@
 					<span class="text-center text-sm italic">(learn &amp; live)</span>
 				</div>
 
-				<ul class="flex place-items-center">
+				<!-- <ul class="flex place-items-center">
 					<li class="flex-auto text-center">
 					<a href="#">search</a>
 					</li>
-				</ul>
+				</ul> -->
 			</nav>
 		</div>
 	</header>
 
 	<?php if ( is_front_page() ) : ?>
-		<main class="container px-4 mx-auto grid gap-8 grid-cols-6 md:px-20">
-	<?php elseif (is_page( array( 'code', 'food' ) )): ?>
-		<main class="container px-4 mx-auto grid gap-8 grid-cols-3 md:px-20">
+		<main class="container px-4 mx-auto md:px-20">
 	<?php else : ?>
 		<main class="container px-4 mx-auto md:px-20">
 	<?php endif; ?>
